7d9b01e0716eb2abd179643349435dc6db958161
Arko <arkoroyb@gmail.com>
2023-07-26 00:13:13 +0600

    project setup and a simple layout created

---
+++ src/components/layouts/RootLayout.js
@@ -0,0 +1,76 @@
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import Link from "next/link";
+const { Header, Content, Footer } = Layout;
+
+const RootLayout = ({ children }) => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout className="layout">
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          className="demo-logo"
+          style={{
+            color: "Red",
+            fontWeight: "700",
+          }}
+        >
+          BLOG
+        </div>
+        <div style={{
+            display: "flex"
+            
+        }}>
+          <Link style={{
+            marginRight: "10px"
+          }} href="/">Home</Link>
+          <Link style={{
+            marginRight: "10px"
+          }} href="/">About</Link>
+          <Link style={{
+            marginRight: "10px"
+          }} href="/">Profile</Link>
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: "",
+        }}
+      >
+        {/* <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb> */}
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          {children}
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Blog Site ©2023 Created by Arko Roy Badhon
+      </Footer>
+    </Layout>
+  );
+};
+
+export default RootLayout;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
-import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function RootLayout({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(<Component {...pageProps} />);
 }
